971e1c6d2a663eff9385a707a96844205bb299c9
Jeremy Odell <jeremy0dell@yahoo.com>
2018-01-01 23:19:26 -0600

    set up HOCs

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,5 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
+import concat from 'lodash/concat'
 
-const App = () => <h1>Hello React!</h1>
+import ToDupe from './ToDupe'
+import withRandomPosition from '../containers/withRandomPosition'
+import withSpinning from '../containers/WithSpinning'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      components: []
+    }
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    const NewDupe = withSpinning(withRandomPosition(ToDupe))
+
+    this.setState({
+      components: concat(this.state.components, <NewDupe key={this.state.components.length} />)
+    })
+  }
+
+  render() {
+    const { components } = this.state
+
+    return (
+      <div>
+        <h1>Hello React!</h1>
+        {components}
+        <button onClick={this.handleClick}>Click for new dupes!!</button>
+      </div>
+    )
+  }
+}
 
 export default App
